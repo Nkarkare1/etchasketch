321ede807dfb605c185b8b973f98843fb5e32c1b
Nkarkare1 <nckarkare@gmail.com>
2025-02-23 22:44:52 -0500

    make the hover colorful

--- script.js
+++ script.js
@@ -5,7 +5,15 @@ for (i = 0; i < 16; i++) {
         // cell.textContent = `cell_${i}_${j}`
         cell.style.height = '50px';
         cell.style.width = '50px';
-
+        cell.addEventListener('mouseenter', (e) => changeCellToRandomColor(e))
         container.appendChild(cell)
     }
 }
+
+function changeCellToRandomColor(event) {
+    event.target.style.backgroundColor = getRandomColor()
+}
+
+function getRandomColor() {
+    return '#' + Math.floor(Math.random()*16777215).toString(16);
+  }
